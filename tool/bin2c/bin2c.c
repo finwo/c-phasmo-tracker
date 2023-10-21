85cd3b9832b7de06b01c48110701254c69525657
Yersa Nordman <yersa@finwo.nl>
2023-10-21 16:48:55 +0200

    Added bin2c tool

---
+++ tool/bin2c/bin2c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int c;
+    while((c = fgetc(stdin)) != EOF) {
+        printf("'\\x%X',", (unsigned)c);
+    }
+    printf("'\\0'"); // put terminating zero
+}
+
