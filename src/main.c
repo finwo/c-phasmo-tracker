e644e5c5436efe84ca70134fa152fb9b9bb5edef
Yersa Nordman <yersa@finwo.nl>
2023-10-21 14:56:02 +0200

    Re-attempt basic example's initializer

--- src/main.c
+++ src/main.c
@@ -2,9 +2,22 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #include "webview/webview.h"
 
+#ifdef _WIN32
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine,
+                   int nCmdShow) {
+  (void)hInst;
+  (void)hPrevInst;
+  (void)lpCmdLine;
+  (void)nCmdShow;
+#else
 int main() {
+#endif
   webview_t w = webview_create(0, NULL);
   webview_set_title(w, "Basic Example");
   webview_set_size(w, 480, 320, WEBVIEW_HINT_NONE);
