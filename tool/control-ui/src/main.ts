0305e2dc76fa9e11a90ea28744cf25a75ad5df5f
Yersa Nordman <yersa@finwo.nl>
2023-12-25 16:53:08 +0100

    Re-enable ui, log to see whether js thread survives

--- tool/control-ui/src/main.ts
+++ tool/control-ui/src/main.ts
@@ -1,84 +1,86 @@
 console.log('Hello from main ts?');
 
-// import 'onsenui/css/onsenui.css';
-// import 'onsenui/css/onsen-css-components.css';
-// import 'onsenui/js/onsenui.js'
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+import 'onsenui/js/onsenui.js'
 
-// function replaceScripts(subject) {
-//   const queue = [subject];
-//   while(queue.length) {
-//     const element = queue.shift();
-//     switch(element.tagName.toLowerCase()) {
-//       case 'script':
-//         const oldScriptEl = element;
-//         const newScriptEl = document.createElement('script');
-//         Array.from(oldScriptEl.attributes).forEach(attr => {
-//           newScriptEl.setAttribute(attr.name, attr.value);
-//         });
-//         const scriptText = document.createTextNode(oldScriptEl.innerHTML);
-//         newScriptEl.appendChild(scriptText);
-//         oldScriptEl.parentNode.replaceChild(newScriptEl, oldScriptEl);
-//         break;
-//       case 'template':
-//         queue.push(...Array.from(element.content.querySelectorAll('template')));
-//         queue.push(...Array.from(element.content.querySelectorAll('script')));
-//         break;
-//       default:
-//         queue.push(...Array.from(element.querySelectorAll('template')));
-//         queue.push(...Array.from(element.querySelectorAll('script')));
-//         break;
-//     }
-//   }
-// }
+function replaceScripts(subject) {
+  const queue = [subject];
+  while(queue.length) {
+    const element = queue.shift();
+    switch(element.tagName.toLowerCase()) {
+      case 'script':
+        const oldScriptEl = element;
+        const newScriptEl = document.createElement('script');
+        Array.from(oldScriptEl.attributes).forEach(attr => {
+          newScriptEl.setAttribute(attr.name, attr.value);
+        });
+        const scriptText = document.createTextNode(oldScriptEl.innerHTML);
+        newScriptEl.appendChild(scriptText);
+        oldScriptEl.parentNode.replaceChild(newScriptEl, oldScriptEl);
+        break;
+      case 'template':
+        queue.push(...Array.from(element.content.querySelectorAll('template')));
+        queue.push(...Array.from(element.content.querySelectorAll('script')));
+        break;
+      default:
+        queue.push(...Array.from(element.querySelectorAll('template')));
+        queue.push(...Array.from(element.querySelectorAll('script')));
+        break;
+    }
+  }
+}
 
-// // Heavily based on https://stackoverflow.com/a/47614491
-// function appendHTML(target, html) {
-//   const wrapper = document.createElement('div');
-//   wrapper.innerHTML = html;
-//   replaceScripts(wrapper);
-//   Array.from(wrapper.children)
-//     .forEach(htmlEl => target.appendChild(htmlEl));
-// }
+// Heavily based on https://stackoverflow.com/a/47614491
+function appendHTML(target, html) {
+  const wrapper = document.createElement('div');
+  wrapper.innerHTML = html;
+  replaceScripts(wrapper);
+  Array.from(wrapper.children)
+    .forEach(htmlEl => target.appendChild(htmlEl));
+}
 
-// function appendTemplate(target, html, name) {
-//   appendHTML(target, `<template id="${name}">${html}</template>`);
-// }
+function appendTemplate(target, html, name) {
+  appendHTML(target, `<template id="${name}">${html}</template>`);
+}
 
-// // Load the main template
-// import tmpl_app from "./app.html";
-// appendHTML(document.body, tmpl_app);
+// Load the main template
+import tmpl_app from "./app.html";
+appendHTML(document.body, tmpl_app);
 
-// import tmpl_tabbar from  "./component/tabbar.html";
-// appendTemplate(document.body, tmpl_tabbar, "tabbar.html");
+import tmpl_tabbar from  "./component/tabbar.html";
+appendTemplate(document.body, tmpl_tabbar, "tabbar.html");
 
-// import tmpl_home from  "./page/home.html";
-// appendTemplate(document.body, tmpl_home, "home.html");
+import tmpl_home from  "./page/home.html";
+appendTemplate(document.body, tmpl_home, "home.html");
 
-// import tmpl_settings from "./page/settings.html";
-// appendTemplate(document.body, tmpl_settings, "settings.html");
+import tmpl_settings from "./page/settings.html";
+appendTemplate(document.body, tmpl_settings, "settings.html");
 
-// window.fn = {};
+window.fn = {};
 
-// window.fn.loadView = function (index) {
-//   document.getElementById('appTabbar').setActiveTab(index);
-// };
+window.fn.loadView = function (index) {
+  document.getElementById('appTabbar').setActiveTab(index);
+};
 
-// window.fn.loadLink = function (url) {
-//   window.open(url, '_blank');
-// };
+window.fn.loadLink = function (url) {
+  window.open(url, '_blank');
+};
 
-// window.fn.pushPage = function (page, anim) {
-//   if (anim) {
-//     document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title }, animation: anim });
-//   } else {
-//     document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title } });
-//   }
-// };
+window.fn.pushPage = function (page, anim) {
+  if (anim) {
+    document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title }, animation: anim });
+  } else {
+    document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title } });
+  }
+};
 
-// if (window._getSettings) {
-//   window._getSettings().then(data => console.log('getSettings done', JSON.stringify(data, null, 2)));
-// }
+if (window._getSettings) {
+  window._getSettings().then(data => console.log('getSettings done', JSON.stringify(data, null, 2)));
+}
 
-// if (window._setSettings) {
-//   window._setSettings({ foo: 'bar' }).then(() => console.log('setSettings done'));
-// }
+if (window._setSettings) {
+  window._setSettings({ foo: 'bar' }).then(() => console.log('setSettings done'));
+}
+
+console.log('I\'m a survivor!!!');
