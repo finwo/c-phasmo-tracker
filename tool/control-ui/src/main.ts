af565fd1a8665898b96d2593369293bbc6217fcc
Yersa Nordman <yersa@finwo.nl>
2024-01-01 23:15:08 +0100

    Back to a hello world to see if that works on windows

--- tool/control-ui/src/main.ts
+++ tool/control-ui/src/main.ts
@@ -1,94 +1,96 @@
-import 'onsenui/css/onsenui.css';
-import 'onsenui/css/onsen-css-components.css';
+document.body.innerHTML += 'Hello World';
 
-// Make it global
-import ons from 'onsenui/js/onsenui.js';
-window.ons = ons;
+// import 'onsenui/css/onsenui.css';
+// import 'onsenui/css/onsen-css-components.css';
 
-function replaceScripts(subject) {
-  const queue = [subject];
-  while(queue.length) {
-    const element = queue.shift();
-    switch(element.tagName.toLowerCase()) {
-      case 'script':
-        const oldScriptEl = element;
-        const newScriptEl = document.createElement('script');
-        Array.from(oldScriptEl.attributes).forEach(attr => {
-          newScriptEl.setAttribute(attr.name, attr.value);
-        });
-        const scriptText = document.createTextNode(oldScriptEl.innerHTML);
-        newScriptEl.appendChild(scriptText);
-        oldScriptEl.parentNode.replaceChild(newScriptEl, oldScriptEl);
-        break;
-      case 'template':
-        queue.push(...Array.from(element.content.querySelectorAll('template')));
-        queue.push(...Array.from(element.content.querySelectorAll('script')));
-        break;
-      default:
-        queue.push(...Array.from(element.querySelectorAll('template')));
-        queue.push(...Array.from(element.querySelectorAll('script')));
-        break;
-    }
-  }
-}
+// // Make it global
+// import ons from 'onsenui/js/onsenui.js';
+// window.ons = ons;
 
-// Heavily based on https://stackoverflow.com/a/47614491
-function appendHTML(target, html) {
-  const wrapper = document.createElement('div');
-  wrapper.innerHTML = html;
-  replaceScripts(wrapper);
-  Array.from(wrapper.children)
-    .forEach(htmlEl => target.appendChild(htmlEl));
-}
+// function replaceScripts(subject) {
+//   const queue = [subject];
+//   while(queue.length) {
+//     const element = queue.shift();
+//     switch(element.tagName.toLowerCase()) {
+//       case 'script':
+//         const oldScriptEl = element;
+//         const newScriptEl = document.createElement('script');
+//         Array.from(oldScriptEl.attributes).forEach(attr => {
+//           newScriptEl.setAttribute(attr.name, attr.value);
+//         });
+//         const scriptText = document.createTextNode(oldScriptEl.innerHTML);
+//         newScriptEl.appendChild(scriptText);
+//         oldScriptEl.parentNode.replaceChild(newScriptEl, oldScriptEl);
+//         break;
+//       case 'template':
+//         queue.push(...Array.from(element.content.querySelectorAll('template')));
+//         queue.push(...Array.from(element.content.querySelectorAll('script')));
+//         break;
+//       default:
+//         queue.push(...Array.from(element.querySelectorAll('template')));
+//         queue.push(...Array.from(element.querySelectorAll('script')));
+//         break;
+//     }
+//   }
+// }
 
-function appendTemplate(target, html, name) {
-  appendHTML(target, `<template id="${name}">${html}</template>`);
-}
+// // Heavily based on https://stackoverflow.com/a/47614491
+// function appendHTML(target, html) {
+//   const wrapper = document.createElement('div');
+//   wrapper.innerHTML = html;
+//   replaceScripts(wrapper);
+//   Array.from(wrapper.children)
+//     .forEach(htmlEl => target.appendChild(htmlEl));
+// }
 
-// Load the main template
-import tmpl_app from "./app.html";
-appendHTML(document.body, tmpl_app);
+// function appendTemplate(target, html, name) {
+//   appendHTML(target, `<template id="${name}">${html}</template>`);
+// }
 
-import tmpl_tabbar from  "./component/tabbar.html";
-appendTemplate(document.body, tmpl_tabbar, "tabbar.html");
+// // Load the main template
+// import tmpl_app from "./app.html";
+// appendHTML(document.body, tmpl_app);
 
-import tmpl_home from  "./page/home.html";
-appendTemplate(document.body, tmpl_home, "home.html");
+// import tmpl_tabbar from  "./component/tabbar.html";
+// appendTemplate(document.body, tmpl_tabbar, "tabbar.html");
 
-import tmpl_settings from "./page/settings.html";
-appendTemplate(document.body, tmpl_settings, "settings.html");
+// import tmpl_home from  "./page/home.html";
+// appendTemplate(document.body, tmpl_home, "home.html");
 
-window.fn = {};
+// import tmpl_settings from "./page/settings.html";
+// appendTemplate(document.body, tmpl_settings, "settings.html");
 
-window.fn.loadView = function (index) {
-  document.getElementById('appTabbar').setActiveTab(index);
-};
+// window.fn = {};
 
-window.fn.loadLink = function (url) {
-  window.open(url, '_blank');
-};
+// window.fn.loadView = function (index) {
+//   document.getElementById('appTabbar').setActiveTab(index);
+// };
 
-window.fn.pushPage = function (page, anim) {
-  if (anim) {
-    document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title }, animation: anim });
-  } else {
-    document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title } });
-  }
-};
+// window.fn.loadLink = function (url) {
+//   window.open(url, '_blank');
+// };
 
-window.appSettings = {
-  get channel() {
-    return new Promise(async resolve => {
-      const allSettings = await _getSettings();
-      resolve((allSettings||{}).channel || '');
-    });
-  },
-  set channel(value) {
-    return new Promise(async resolve => {
-      const allSettings = await _getSettings();
-      allSettings.channel = value;
-      _setSettings(allSettings);
-      resolve();
-    });
-  }
-};
+// window.fn.pushPage = function (page, anim) {
+//   if (anim) {
+//     document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title }, animation: anim });
+//   } else {
+//     document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title } });
+//   }
+// };
+
+// window.appSettings = {
+//   get channel() {
+//     return new Promise(async resolve => {
+//       const allSettings = await _getSettings();
+//       resolve((allSettings||{}).channel || '');
+//     });
+//   },
+//   set channel(value) {
+//     return new Promise(async resolve => {
+//       const allSettings = await _getSettings();
+//       allSettings.channel = value;
+//       _setSettings(allSettings);
+//       resolve();
+//     });
+//   }
+// };
