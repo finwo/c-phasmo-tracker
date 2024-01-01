2cba694bf7fe60e87a6a518625b8b810be127011
Yersa Nordman <yersa@finwo.nl>
2024-01-01 23:20:29 +0100

    Scream hello, origin hello worked

--- tool/control-ui/src/main.ts
+++ tool/control-ui/src/main.ts
@@ -1,51 +1,51 @@
-document.body.innerHTML += 'Hello World';
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
 
-// import 'onsenui/css/onsenui.css';
-// import 'onsenui/css/onsen-css-components.css';
+// Make it global
+import ons from 'onsenui/js/onsenui.js';
+window.ons = ons;
 
-// // Make it global
-// import ons from 'onsenui/js/onsenui.js';
-// window.ons = ons;
+function replaceScripts(subject) {
+  const queue = [subject];
+  while(queue.length) {
+    const element = queue.shift();
+    switch(element.tagName.toLowerCase()) {
+      case 'script':
+        const oldScriptEl = element;
+        const newScriptEl = document.createElement('script');
+        Array.from(oldScriptEl.attributes).forEach(attr => {
+          newScriptEl.setAttribute(attr.name, attr.value);
+        });
+        const scriptText = document.createTextNode(oldScriptEl.innerHTML);
+        newScriptEl.appendChild(scriptText);
+        oldScriptEl.parentNode.replaceChild(newScriptEl, oldScriptEl);
+        break;
+      case 'template':
+        queue.push(...Array.from(element.content.querySelectorAll('template')));
+        queue.push(...Array.from(element.content.querySelectorAll('script')));
+        break;
+      default:
+        queue.push(...Array.from(element.querySelectorAll('template')));
+        queue.push(...Array.from(element.querySelectorAll('script')));
+        break;
+    }
+  }
+}
 
-// function replaceScripts(subject) {
-//   const queue = [subject];
-//   while(queue.length) {
-//     const element = queue.shift();
-//     switch(element.tagName.toLowerCase()) {
-//       case 'script':
-//         const oldScriptEl = element;
-//         const newScriptEl = document.createElement('script');
-//         Array.from(oldScriptEl.attributes).forEach(attr => {
-//           newScriptEl.setAttribute(attr.name, attr.value);
-//         });
-//         const scriptText = document.createTextNode(oldScriptEl.innerHTML);
-//         newScriptEl.appendChild(scriptText);
-//         oldScriptEl.parentNode.replaceChild(newScriptEl, oldScriptEl);
-//         break;
-//       case 'template':
-//         queue.push(...Array.from(element.content.querySelectorAll('template')));
-//         queue.push(...Array.from(element.content.querySelectorAll('script')));
-//         break;
-//       default:
-//         queue.push(...Array.from(element.querySelectorAll('template')));
-//         queue.push(...Array.from(element.querySelectorAll('script')));
-//         break;
-//     }
-//   }
-// }
+// Heavily based on https://stackoverflow.com/a/47614491
+function appendHTML(target, html) {
+  const wrapper = document.createElement('div');
+  wrapper.innerHTML = html;
+  replaceScripts(wrapper);
+  Array.from(wrapper.children)
+    .forEach(htmlEl => target.appendChild(htmlEl));
+}
 
-// // Heavily based on https://stackoverflow.com/a/47614491
-// function appendHTML(target, html) {
-//   const wrapper = document.createElement('div');
-//   wrapper.innerHTML = html;
-//   replaceScripts(wrapper);
-//   Array.from(wrapper.children)
-//     .forEach(htmlEl => target.appendChild(htmlEl));
-// }
+function appendTemplate(target, html, name) {
+  appendHTML(target, `<template id="${name}">${html}</template>`);
+}
 
-// function appendTemplate(target, html, name) {
-//   appendHTML(target, `<template id="${name}">${html}</template>`);
-// }
+appendHTML(document.body, `<h1>Hello World!!</h1>`);
 
 // // Load the main template
 // import tmpl_app from "./app.html";
