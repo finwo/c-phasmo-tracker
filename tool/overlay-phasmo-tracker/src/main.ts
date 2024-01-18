624f9ffb3f78533b60ff2103202f7be5a3465688
Yersa Nordman <yersa@finwo.nl>
2024-01-18 22:09:29 +0100

    Simplified command splitter for phasmo tracker

--- tool/overlay-phasmo-tracker/src/main.ts
+++ tool/overlay-phasmo-tracker/src/main.ts
@@ -1,15 +1,35 @@
 import { Client } from '../../client-jerry/src/index.ts';
-import { parse } from 'shell-quote';
 
+const commands = {
+  reset() {
+    console.log('RESET CALLED');
+  },
+  fingies() {
+    console.log('FINGIES CALLED');
+  },
+};
+
+// Ingest commands
 const client = new Client("/topic/chat");
-client.addListener(async ({channel, tags, message}) => {
-  console.log(JSON.stringify({channel,tags,message},null,2));
+client.addListener(async ({self, channel, tags, message}) => {
+  if (message.substring(0,1) !== '!') return;
 
-  // Check if the chatter has the right permissions
-  const hasPerms = (('#'+tags.username) == channel) || tags.mod;
-  if (!hasPerms) return;
+  // Command basics
+  const argv = message.substring(1).split(' ');
+  if ('function' !== typeof commands[argv[0]]) {
+    return;
+  }
 
-  const argv = parse(message);
-  console.log(argv);
+  // Call it, it can decide perms for itself
+  commands[argv[0]](...(argv.slice(1)));
+
+  // console.log(JSON.stringify({channel,tags,message},null,2));
+
+  // // Check if the chatter has the right permissions
+  // const hasPerms = (('#'+tags.username) == channel) || tags.mod;
+  // if (!hasPerms) return;
+
+  // const argv = parse(message);
+  // console.log(argv);
 
 });
